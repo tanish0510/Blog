3d504c1e54f1623fbc9d9741622488b2836d0245
tanish0510 <tanishseth0510@gmail.com>
2025-03-30 22:45:01 +0530

    eslint--fix

--- blog-app/app/admin/layout.jsx
+++ blog-app/app/admin/layout.jsx
@@ -2,7 +2,7 @@ import { assets } from "@/Assets/assets";
 import Sidebar from "@/Components/AdminComponents/Sidebar";
 import Image from "next/image";
 import { ToastContainer} from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({ children }) {
     return (
